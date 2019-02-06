5a4d71a480e1a7b2e7c75b758fc3e2c439d0621c
djohnson58 <46780175+djohnson58@users.noreply.github.com>
2019-02-06 14:31:01 -0800

    Added Mult Class

---
+++ src/Term2Mult.java
@@ -0,0 +1,12 @@
+
+public class Term2Mult {
+	public static void main(String[] args) {
+		int result  = mult(5, 32);
+        System.out.println("5 * 32 is: " + result);
+    }
+	
+	public int mult(int a, int b) {
+		return a * b;
+	}
+}
+
